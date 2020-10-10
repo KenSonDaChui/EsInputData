b492810bf824ef4c941802da3bb3df6c6449ddb6
xutongxue <songhu_kenson@163.com>
2020-10-10 10:22:03 +0800

    单一环境ES(导入Mysql数据并查询)

---
+++ src/main/java/com/jiucheng/EsApplication.java
@@ -0,0 +1,17 @@
+package com.jiucheng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * com.jiucheng.EsApplication
+ *
+ * @auther qiaoba
+ * @date 2020/10/10 0010 9:33
+ **/
+@SpringBootApplication
+public class EsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EsApplication.class,args);
+    }
+}
